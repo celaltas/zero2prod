9c2f0074a6ab81d6553175ba4eebf22b1cc02f50
celaltas <60509470+celaltas@users.noreply.github.com>
2024-02-14 01:14:33 +0300

    subscription endpoint added with  it's tests together

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,7 +7,7 @@ async fn test_health_check() {
     let addrs = spawn_app();
     let client = Client::new();
     let response = client
-        .get(&format!("http://{}/health_check", &addrs))
+        .get(&format!("{}/health_check", &addrs))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -21,5 +21,50 @@ fn spawn_app() -> String {
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
     tokio::spawn(server);
-    format!("127.0.0.1:{}", port)
+    format!("http://127.0.0.1:{}", port)
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let addrs = spawn_app();
+    let client = Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &addrs))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request post request to subscriptions endpoint.");
+
+    assert_eq!(200, response.status().as_u16())
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let addrs = spawn_app();
+    let client = Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &addrs))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request post request to subscriptions endpoint.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
 }
