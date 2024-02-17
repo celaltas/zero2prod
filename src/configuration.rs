32680a5de20cc94246b0ba480459c04785c127df
celaltas <60509470+celaltas@users.noreply.github.com>
2024-02-17 13:37:07 +0300

    dockerfile added and config divided for envs

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,12 @@
 use config::{Config, File, FileFormat};
 
 #[derive(serde::Deserialize, Debug)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+#[derive(serde::Deserialize, Debug)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
@@ -27,15 +33,59 @@ impl DatabaseSettings {
 #[derive(serde::Deserialize, Debug)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let mut builder = Config::builder();
-    builder = builder.add_source(File::new("configuration", FileFormat::Yaml));
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let env: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
+    let base_conf_path = base_path.join("configuration/base");
+    let env_conf_path = base_path.join(format!("configuration/{}", env.as_str()));
+
+    let builder = Config::builder()
+        .add_source(File::new(
+            base_conf_path.to_str().unwrap(),
+            FileFormat::Yaml,
+        ))
+        .add_source(File::new(env_conf_path.to_str().unwrap(), FileFormat::Yaml));
 
     match builder.build() {
         Ok(config) => config.try_deserialize::<Settings>(),
         Err(e) => Err(e),
     }
 }
+
+
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,18 @@ use zero2prod::{
     telemetry::{get_subscriber, init_subscriber},
 };
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod", "info", std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     let dsn = configuration.database.connection_string();
-    let connection_pool = PgPool::connect(&dsn)
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPool::connect_lazy(&dsn).expect("Failed to connect to Postgres.");
     run(listener, connection_pool)?.await
 }
